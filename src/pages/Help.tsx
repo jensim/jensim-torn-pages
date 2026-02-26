71d76fdbef99c7d8a1bc80a77b339d3b78522d7e
Jens Brimfors <jens.alexander.brimfors@wolt.com>
2026-02-26 10:31:22 +0100

    Fix Help page url

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -5,7 +5,7 @@ const Help: React.FC = () => {
     <div className="App-header">
       <h1>Help</h1>
       <p>This is the Help page.</p>
-      <p><a href="github.com/jensim/jensim-torn-pages/">Github Repository</a></p>
+      <p><a href="https://github.com/jensim/jensim-torn-pages/">Github Repository</a></p>
     </div>
   );
 };
